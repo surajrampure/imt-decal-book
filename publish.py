abdb7a3a8e0abec213f240795d8f425e0d3f75c0
Suraj Rampure <surajr@me.com>
2018-05-28 19:13:04 -0400

    updating book

--- publish.py
+++ publish.py
@@ -2,3 +2,10 @@ import os
 
 os.system("markdown-folder-to-html src")
 os.system("mv _src docs")
+
+arg = os.sys.argv[1]
+
+if arg == '-c':
+	os.system("git add .")
+	os.system("git commit -m 'updating book'")
+	os.system("git push")
